2aaa07dc1c29ca9167f7af8baac0dc666ede46a4
Igor Baranov <baranow.ig@gmail.com>
2024-10-07 23:42:34 +0200

    Add data from API with Redux Toolkit Thunk

--- src/common/ui/constants.js
+++ src/common/ui/constants.js
@@ -1,1 +1,2 @@
 export const MOVIEDB_IMAGES_URL = "https://image.tmdb.org/t/p/original";
+export const MOVIEDB_API_URL = "https://api.themoviedb.org/3";
--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -1,16 +1,45 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import MovieCard from "./MovieCard";
-import { Heading, SimpleGrid } from "@chakra-ui/react";
+import { Flex, Heading, SimpleGrid, Spinner, useToast } from "@chakra-ui/react";
 import { MOVIEDB_IMAGES_URL } from "src/common/ui/constants";
-import { selectAllMovies } from "./moviesSlice";
+import {
+  fetchMovies,
+  selectAllMovies,
+  selectMoviesError,
+  selectMoviesStatus,
+} from "./moviesSlice";
+import { useEffect } from "react";
 
 function MoviesList() {
+  const dispatch = useDispatch();
+  const toast = useToast();
   const movies = useSelector(selectAllMovies); //passed function has access to whole state of my whole app and then I just say what exactly I want to take from my state. In the case I am taking movies, where I basically have the list of movies. This name is specify is the store.
-  return (
-    <>
-      <Heading textAlign="center" size="xl" mb={6}>
-        Trending Movies
-      </Heading>
+  const moviesStatus = useSelector(selectMoviesStatus);
+  const moviesError = useSelector(selectMoviesError);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchMovies()).unwrap();
+      } catch (err) {
+        toast({
+          title: "Failed to load movies",
+          description:
+            err.message ||
+            "Please refresh the page and check your internet connection!",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    };
+
+    fetchData();
+  }, [dispatch, toast]);
+
+  let content;
+  if (moviesStatus === "succeeded") {
+    content = (
       <SimpleGrid columns={{ sm: 1, md: 3, lg: 4 }} spacing={4}>
         {movies.map((movie) => (
           <MovieCard
@@ -22,6 +51,27 @@ function MoviesList() {
           />
         ))}
       </SimpleGrid>
+    );
+  } else if (moviesStatus === "loading") {
+    content = (
+      <Flex alignItems="center" justifyContent="center" minH="100vh">
+        <Spinner text="Loading..." />
+      </Flex>
+    );
+  } else if (moviesStatus === "failed") {
+    content = (
+      <Flex alignItems="center" justifyContent="center" minH="100vh">
+        {moviesError}
+      </Flex>
+    );
+  }
+
+  return (
+    <>
+      <Heading textAlign="center" size="xl" mb={6}>
+        Trending Movies
+      </Heading>
+      {content}
     </>
   );
 }
